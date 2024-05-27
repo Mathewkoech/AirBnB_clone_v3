30375eaf576ae5809024775b684bf124f7f5c0d9
mathewkoech <mathewkoech55@gmail.com>
2024-05-27 06:40:03 -0700

    create api views

---
+++ api/v1/app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+flask app
+"""
+from flask import Flask, make_response, jsonify
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def call(self):
+    """ calls storage.close()"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def error(error):
+    """ handles 404 error and gives a json response"""
+    return make_response(jsonify({'error': "Not found"}), 404)
+
+
+if __name__ == '__main__':
+    if getenv("HBNB_API_HOST") is None:
+        HBNB_API_HOST = '0.0.0.0'
+    else:
+        HBNB_API_HOST = getenv("HBNB_API_HOST")
+    if getenv("HBNB_API_PORT") is None:
+        HBNB_API_PORT = 5000
+    else:
+        HBNB_API_PORT = int(getenv("HBNB_API_PORT"))
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
